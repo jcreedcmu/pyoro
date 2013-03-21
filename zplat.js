1f20d0b7a49081284f1f23c43c5e0d125313ae25
Jason Reed <jcreed@gmail.com>
2013-03-21 16:45:25 -0400

    model

--- zplat.js
+++ zplat.js
@@ -4,50 +4,38 @@ function int(x) {
   return Math.floor(x);
 }
 
-var NUM_TILES_X = 12;
-var NUM_TILES_Y = 9;
+var NUM_TILES_X = 16;
+var NUM_TILES_Y = 12;
 var TILE_SIZE = 16;
-var SCALE = 4;
+var SCALE = 3;
 
-var model = {};
 var view = {};
 
-function imgProm(src) {
-  var def = Q.defer();
-  var sprite = new Image();
-  sprite.src = "sprite.png";
-  sprite.onload = function() { def.resolve(sprite); }
-  return def.promise;
+var model;
+
+var sprites = {
+  box:    {x:1, y:4},
+  white:  {x:0, y:0},
+  player: {x:1, y:2},
+};
+
+function bindVia(obj, proto) {
+  var fs = _.functions(proto);
+  console.log(fs);
+  _.each(fs, function(f) {
+    obj[f] = function() {
+      return proto[f].apply(obj, arguments);
+    }
+  });
 }
 
-function go() {
-  var c = $("<canvas>")[0];
-  $("body").append(c);
-
-  var d = c.getContext('2d');
-
-  view.c = c;
-  view.d = d;
-  imgProm('sprite.png').then(function(s) {
-    view.sprite = s;
-  }).then(draw_model);
-
+function Model(props) {
+  _.extend(this, props);
+  bindVia(this, Model.prototype);
+  console.log(_.methods(this));
 }
 
-function draw_sprite(d, sprite_loc, wpos) {
-  var center_x = int(ww / 2);
-  var center_y = int(hh / 2);
-
-  var o_x = center_x - int(NUM_TILES_X * TILE_SIZE * SCALE / 2);
-  var o_y = center_y - int(NUM_TILES_Y * TILE_SIZE * SCALE / 2);
-
-  d.webkitImageSmoothingEnabled = false;
-  d.drawImage(view.sprite,
-	      sprite_loc.x * TILE_SIZE, sprite_loc.y * TILE_SIZE, TILE_SIZE, TILE_SIZE,
-	      o_x + wpos.x * TILE_SIZE * SCALE, o_y + wpos.y * TILE_SIZE * SCALE, TILE_SIZE * SCALE, TILE_SIZE * SCALE);
-}
-
-function draw_model() {
+Model.prototype.draw = function (msg) {
   var c = view.c;
   var d = view.d;
 
@@ -61,14 +49,64 @@ function draw_model() {
 
   for (var y = 0; y < NUM_TILES_Y; y++) {
     for (var x = 0; x < NUM_TILES_X; x++) {
-      draw_sprite(d, {x:0, y:0},{x:x, y:y});
+      draw_sprite(d, this.getTile(x + this.viewPort.x, y + this.viewPort.y), {x:x, y:y});
     }
   }
 
-  draw_sprite(d, {x:1, y:2},{x:1, y:5});
-  for (var x = 0; x < NUM_TILES_X; x++) {
-    draw_sprite(d, {x:1, y:4},{x:x, y:6});
-  }
+  draw_sprite(d, 'player', {x:this.player.pos.x - this.viewPort.x,
+			    y:this.player.pos.y - this.viewPort.y});
+
+
 }
 
-$(window).resize(draw_model);
+Model.prototype.getTile = function (x,y) {
+  if (y == 0)
+    return 'box';
+  else return 'white';
+}
+
+function imgProm(src) {
+  var def = Q.defer();
+  var sprite = new Image();
+  sprite.src = "sprite.png";
+  sprite.onload = function() { def.resolve(sprite); }
+  return def.promise;
+}
+
+function go() {
+
+  model = new Model({
+    player: {pos: {x: 0, y: -1}},
+    viewPort: {x: -5, y: -5},
+  });
+
+  $(window).resize(model.draw);
+
+  var c = $("<canvas>")[0];
+  $("body").append(c);
+
+  var d = c.getContext('2d');
+
+  view.c = c;
+  view.d = d;
+  imgProm('sprite.png').then(function(s) {
+    view.sprite = s;
+  }).then(model.draw)
+    .done();
+
+}
+
+function draw_sprite(d, sprite_id, wpos) {
+  var center_x = int(ww / 2);
+  var center_y = int(hh / 2);
+
+  var o_x = center_x - int(NUM_TILES_X * TILE_SIZE * SCALE / 2);
+  var o_y = center_y - int(NUM_TILES_Y * TILE_SIZE * SCALE / 2);
+
+  var sprite_loc = sprites[sprite_id];
+
+  d.webkitImageSmoothingEnabled = false;
+  d.drawImage(view.sprite,
+	      sprite_loc.x * TILE_SIZE, sprite_loc.y * TILE_SIZE, TILE_SIZE, TILE_SIZE,
+	      o_x + wpos.x * TILE_SIZE * SCALE, o_y + wpos.y * TILE_SIZE * SCALE, TILE_SIZE * SCALE, TILE_SIZE * SCALE);
+}
