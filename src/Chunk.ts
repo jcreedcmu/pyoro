a04cb95a4fd786ce9ae8a58407e83b4c2cef82a0
Jason Reed <jcreed@gmail.com>
2018-09-18 15:17:22 -0400

    more types

--- src/Chunk.ts
+++ src/Chunk.ts
@@ -1,9 +1,12 @@
-import { hash, vplus } from './util';
 import * as _ from 'underscore';
+import { hash, vplus } from './util';
+import { Dict, Point } from './types';
 
 export const CHUNK_SIZE = 16; // in number of tiles, for purposes of caching
 
-function rawGetTile(p) {
+type Tile = 'box' | 'box3' | 'fragile_box' | 'empty';
+
+function rawGetTile(p: Point): Tile {
   var h = hash(p, 2);
   var mtn = h[0] - (p.x * 0.015 + p.y * -0.003);
   if (h[0] - p.y * 0.1 < 0.3 || mtn < 0.3) {
@@ -12,47 +15,56 @@ function rawGetTile(p) {
   else return 'empty';
 }
 
-export function Layer() {
-  this.tiles = {};
+export class Layer {
+  tiles: Dict<Tile> = {};
+
+  getTile(p: Point): Tile {
+    return this.tiles[p.x + ',' + p.y];
+  }
+
+  putTile(p: Point, t: Tile): void {
+    this.tiles[p.x + ',' + p.y] = t;
+  }
+
+  extend(l) {
+    _.extend(this.tiles, l.tiles);
+  }
+
 }
 
-Layer.prototype.getTile = function(p) {
-  return this.tiles[p.x + ',' + p.y];
-}
+export class CompositeLayer extends Layer {
+  l1: Layer;
+  l2: Layer;
 
-Layer.prototype.putTile = function(p, t) {
-  this.tiles[p.x + ',' + p.y] = t;
-}
+  constructor(l1: Layer, l2: Layer) {
+    super();
+    this.l1 = l1;
+    this.l2 = l2;
+  }
 
-Layer.prototype.extend = function(l) {
-  _.extend(this.tiles, l.tiles);
-}
+  putTile(p: Point, t: Tile): void {
+    throw "CompositeLayer is readonly";
+  }
 
-export function CompositeLayer(l1, l2) {
-  this.l1 = l1;
-  this.l2 = l2;
-}
-
-CompositeLayer.prototype = new Layer();
-
-CompositeLayer.prototype.putTile = function(p, t) {
-  throw "CompositeLayer is readonly";
-}
-
-CompositeLayer.prototype.getTile = function(p) {
-  return this.l1.getTile(p) || this.l2.getTile(p);
-}
-
-export function Chunk(p, props) {
-  this.pos = p;
-  this.rawGetTile = rawGetTile;
-  _.extend(this, props);
-  for (var y = 0; y < CHUNK_SIZE; y++) {
-    for (var x = 0; x < CHUNK_SIZE; x++) {
-      var m = vplus(p, { x: x, y: y });
-      this.tiles[m.x + ',' + m.y] = this.rawGetTile(m);
-    }
+  getTile(p: Point): Tile {
+    return this.l1.getTile(p) || this.l2.getTile(p);
   }
 }
 
-Chunk.prototype = new Layer();
+export class Chunk extends Layer {
+  pos: Point;
+  rawGetTile: (p: Point) => Tile;
+
+  constructor(p: Point, props) {
+    super();
+    this.pos = p;
+    this.rawGetTile = rawGetTile;
+    _.extend(this, props);
+    for (var y = 0; y < CHUNK_SIZE; y++) {
+      for (var x = 0; x < CHUNK_SIZE; x++) {
+        var m = vplus(p, { x: x, y: y });
+        this.tiles[m.x + ',' + m.y] = this.rawGetTile(m);
+      }
+    }
+  }
+}
