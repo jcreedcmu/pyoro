2e4308577e58051976d69ba134d3544498f0f129
Jason Reed <jcreed@gmail.com>
2018-10-03 18:17:59 -0400

    clean up some other unused stuff

--- src/layer.ts
+++ src/layer.ts
@@ -1,10 +1,7 @@
-import { rect_intersect, vplus } from './util';
-import { Dict, Point, Tile, Rect } from './types';
+import { Dict, Point, Tile } from './types';
 import { NUM_TILES_X, NUM_TILES_Y } from './constants';
 
 export type TileFunc = (p: Point) => Tile;
-
-export const CHUNK_SIZE = 16; // in number of tiles, for purposes of caching
 
 export type Layer = { tiles: Dict<Tile> };
 
@@ -15,5 +12,3 @@ export function getTile(l: Layer, p: Point): Tile {
 export function putTile(l: Layer, p: Point, t: Tile): void {
   l.tiles[p.x + ',' + p.y] = t;
 }
-
-type PosPt = { pos: Point };
