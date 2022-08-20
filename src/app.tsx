ae7f7dd02f300f836eaf70e7cfe8aba1df04a198
Jason Reed <jcreed@gmail.com>
2022-08-20 17:32:52 -0400

    debugging image loading

--- src/app.tsx
+++ src/app.tsx
@@ -5,6 +5,7 @@ import { keyFromCode } from './key';
 import { Dispatch } from './reduce';
 import { init_state, State } from './state';
 import { CanvasInfo, useCanvas } from './use-canvas';
+import { imgProm } from './util';
 import { drawView } from './view';
 
 type CanvasProps = {
@@ -17,6 +18,7 @@ export function App(props: { dispatch: Dispatch, msg: string }): JSX.Element {
   const { dispatch, msg } = props;
 
   function render(ci: CanvasInfo, props: CanvasProps) {
+    console.log('rendering small canvas');
     const { d, size: { x, y } } = ci;
     d.fillStyle = 'white';
     d.fillRect(0, 0, x, y);
@@ -24,6 +26,9 @@ export function App(props: { dispatch: Dispatch, msg: string }): JSX.Element {
     d.fillText(props.vestigial, 12, 24);
     if (props.spriteImg !== null && props.main.iface.vd !== null)
       drawView({ d, spriteImg: props.spriteImg, vd: props.main.iface.vd }, props.main);
+    else {
+      console.log('not fully loaded:', props.spriteImg, props.main.iface.vd);
+    }
   }
 
   function handleKey(e: KeyboardEvent) {
@@ -57,6 +62,14 @@ export function App(props: { dispatch: Dispatch, msg: string }): JSX.Element {
   const [state, setState] = React.useState(init_state);
   const [canvasState, setCanvasState] = React.useState('hello');
   const [cref, mc] = useCanvas({ vestigial: canvasState, main: state, spriteImg: spriteImg }, render);
+
+  React.useEffect(() => {
+    (async () => {
+      console.log('loading sprite sheet');
+      setSpriteImg(await imgProm('assets/sprite.png'));
+      console.log('loaded sprite sheet');
+    })().catch(console.error);
+  }, []);
 
   // Event handlers
   React.useEffect(() => {
