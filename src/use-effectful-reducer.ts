619e986cf36f707467e88cb845b6fc11a9b700aa
Jason Reed <jcreed@gmail.com>
2022-08-21 17:40:19 -0400

    Fix implementation of use-effectful-reducer

--- src/use-effectful-reducer.ts
+++ src/use-effectful-reducer.ts
@@ -11,38 +11,51 @@ import * as React from 'react';
 // The reducer itself is meant to be a pure computation of a
 // description of the next state and some side effects we should do,
 // as a consequence of an action and the current state.
+
+// Here's a toy example of its intended use.
+// It's a counter that, whenever it reaches 5, has a side effect that it resets 1000ms later.
 //
-// We then use React's own useReducer, slipping in an action data
-// structure that has an extra field which we sneakily update in place
-// with the effect-part of the return value of the reducer, so that by
-// the time the dispatch call is over, we can see the side-effects
-// that we wanted to trigger, and we can call doEffect on them.
+// type Effect = 'delayedReset';
+// type Action = 'inc' | 'reset';
+
+// function (): JSX.Element {
+//   const [counter, dispatch] = useEffectfulReducer(0, reducer, doEffect);
+
+//   function reducer(x: number, action: Action): { state: number, effects?: Effect[] } {
+//     switch (action) {
+//       case 'inc': return { state: x + 1, effects: x + 1 == 5 ? ['delayedReset'] : undefined };
+//       case 'reset': return { state: 0 };
+//     }
+//   }
+
+//   function doEffect(effect: Effect) {
+//     if (effect == 'delayedReset') {
+//       setTimeout(() => dispatch('reset'), 1000);
+//     }
+//   }
+
+//   return <div><button onClick={() => dispatch('inc')}>inc</button>{counter}</div>;
+// }
 
 export type Result<S, E> = { state: S, effects?: E[] };
-
-type ProxyAction<A, S, E> = {
-  action: A,
-  effects?: E[],
-}
 
 export function useEffectfulReducer<A, S, E>(
   initialState: S,
   reduce: (state: S, action: A) => Result<S, E>,
   doEffect: (effect: E) => void):
   [S, (action: A) => void] {
-  function proxyReduce(state: S, proxyAction: ProxyAction<A, S, E>): S {
-    const result = reduce(state, proxyAction.action);
-    proxyAction.effects = result.effects;
-    return result.state;
-  }
-  const [state, dispatch] = React.useReducer<(state: S, action: ProxyAction<A, S, E>) => S>(proxyReduce, initialState);
+  const [state, setState] = React.useState<S>(initialState);
   return [state, (a: A) => {
-    const proxyAction: ProxyAction<A, S, E> = { action: a };
-    dispatch(proxyAction);
-    if (proxyAction.effects !== undefined) {
-      for (const effect of proxyAction.effects) {
-        doEffect(effect);
-      }
-    }
+    setState(prevState => {
+      const result = reduce(prevState, a);
+      setTimeout(() => {
+        if (result.effects !== undefined) {
+          for (const effect of result.effects) {
+            doEffect(effect);
+          }
+        }
+      }, 0);
+      return result.state;
+    });
   }];
 }
