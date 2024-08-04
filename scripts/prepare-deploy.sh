3f2a46153d630b0db842e1fa019e609896097c37
Jason Reed <jcreed@gmail.com>
2024-08-04 14:58:45 -0400

    Fix missing backslash

--- scripts/prepare-deploy.sh
+++ scripts/prepare-deploy.sh
@@ -1,20 +1,16 @@
 rm -rf dist
 mkdir dist
 
-echo "Building bundle..."
 # creates public/js/bundle.js
 make build
 
-echo "Building docs..."
 # creates docs
 npx typedoc src/*.ts
-echo $?
 
-echo "Copying..."
 cp -rv \
   public/index.html \
   public/js \
   public/assets \
   public/style.css \
-  docs/
+  docs/ \
   dist
