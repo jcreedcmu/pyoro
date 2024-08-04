f788941028f799ece8657354154633e5b2da1c27
Jason Reed <jcreed@gmail.com>
2024-08-04 14:55:29 -0400

    Run doc generation during static build (fix #19)

--- scripts/prepare-deploy.sh
+++ scripts/prepare-deploy.sh
@@ -4,9 +4,13 @@ mkdir dist
 # creates public/js/bundle.js
 make build
 
+# creates docs
+npx typedoc src/*.ts
+
 cp -rv \
   public/index.html \
   public/js \
   public/assets \
   public/style.css \
+  docs/
   dist
